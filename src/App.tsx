b7421a52dcc31a699b7c0ffd9a2c6f96304e62f8
lys0611 <dldptmd611@gmail.com>
2024-08-04 23:04:40 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Apply from './routes/Apply';
+import Login from './routes/Login/Login';
 
 const App: React.FC = () => (
     <Router>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/apply" element={<Apply />} />
         </Routes>
     </Router>
