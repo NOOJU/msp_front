b9ad4a5d22e36af792c2b76a9b61332c8d61dbc7
JOONU <junjunjun815@gmail.com>
2024-09-25 19:19:47 +0900

    IP 수정 반영

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 
-// 생성 로직
-export const API_BASE_URL = "http://localhost:8000";
+// 인증서버 로직
+export const API_BASE_URL = "http://61.109.237.118:9000";
 
-// PR 로직
+// 봇서버 로직
 export const API_BASE_URL2 = "http://210.109.52.162:5000";
