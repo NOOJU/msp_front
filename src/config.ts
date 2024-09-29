cf4c28d9e478f047e0ca47ec78703844061cf2fe
lys0611 <dldptmd611@gmail.com>
2024-09-29 20:40:23 +0900

    config.ts http를 https로 변경

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // 인증서버 로직
-export const API_BASE_URL = "http://61.109.237.118:9000";
+export const API_BASE_URL = "https://61.109.237.118:9000";
 // export const API_BASE_URL = "http://localhost:8000";
 
 
 // 봇서버 로직
-export const API_BASE_URL2 = "http://210.109.52.162:5000";
+export const API_BASE_URL2 = "https://210.109.52.162:5000";
 // export const API_BASE_URL2 = "http://localhost:5000";
