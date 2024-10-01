6160242e968c27853bfee7035a345418e18c6376
lys0611 <dldptmd611@gmail.com>
2024-10-01 23:40:05 +0900

    리버스 프록시 설정
    최신 SSL 인증서 사용 명령
    docker run -p 443:443 \
    -v /etc/letsencrypt/archive/syucloud.store/fullchain2.pem:/etc/letsencrypt/live/syucloud.store/fullchain.pem \
    -v /etc/letsencrypt/archive/syucloud.store/privkey2.pem:/etc/letsencrypt/live/syucloud.store/privkey.pem front

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // 인증서버 로직
-// export const API_BASE_URL = "https://61.109.237.118:9000";
+// export const API_BASE_URL = "http://61.109.237.118:9000";
 // export const API_BASE_URL = "http://localhost:8000";
-export const API_BASE_URL = "https://api.syucloud.store";
-
+// export const API_BASE_URL = "https://api.syucloud.store";
+export const API_BASE_URL = "https://www.syucloud.store/api";  // 프록시 설정
 
 // 봇서버 로직
-export const API_BASE_URL2 = "https://210.109.52.162:5000";
+export const API_BASE_URL2 = "http://210.109.52.162:5000";
 // export const API_BASE_URL2 = "http://localhost:5000";
