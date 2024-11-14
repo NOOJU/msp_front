cb2293fd1ec906cc6e21c37cebb195d0e1978166
lys0611 <dldptmd611@gmail.com>
2024-11-14 18:41:53 +0900

    www 붙이고 안 붙이는 거 둘 다

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 // 인증서버 로직
-export const API_BASE_URL = "https://syucloud.store/auth";  // 프록시 설정
-// export const API_BASE_URL = "http://61.109.237.118:9000";
-// export const API_BASE_URL = "http://localhost:8000";
-// export const API_BASE_URL = "https://api.syucloud.store/auth";
-
+export const API_BASE_URL =
+    window.location.hostname === "www.syucloud.store"
+        ? "https://www.syucloud.store/auth"
+        : "https://syucloud.store/auth";
 
 // 봇서버 로직
-export const API_BASE_URL2 = "https://syucloud.store/bot";
-// export const API_BASE_URL2 = "http://localhost:5000";
+export const API_BASE_URL2 =
+    window.location.hostname === "www.syucloud.store"
+        ? "https://www.syucloud.store/bot"
+        : "https://syucloud.store/bot";
