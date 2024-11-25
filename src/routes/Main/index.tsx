a9ddb4fd7df8238795928841f634c00f576eb035
JOONU <junjunjun815@gmail.com>
2024-11-26 01:03:21 +0900

    메인 화면 사진 추가

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -7,7 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
-import { MainPageSlider1, MainPageStyled } from "./styled";
+import { MainPageSlider1, MainPageSlider2, MainPageStyled } from "./styled";
 import { Link } from "react-router-dom";
 import { useTheme } from "styled-components";
 
@@ -16,6 +16,11 @@ const Main: React.FC = () => {
   let sliderRef: Slider | null = null;
   const [onPlay, setOnPlay] = useState<Boolean>(true); //슬라이더의 재생 여부를 가지고 있음
   const isPc = useTheme().isPc;
+
+  // 백그라운드 이미지 패치
+  const vmImagePath = "/image/VM1.webp";
+  const vmMobileImagePath = "/image/VM2.webp";
+  const LoadBalancerImagePath = "/image/LoadBalancer.webp";
 
   const play = () => {
     sliderRef?.slickPlay();
@@ -45,7 +50,7 @@ const Main: React.FC = () => {
   const settings = {
     dots: true, // 도트
     infinite: true, // 무한
-    speed: 3000, // 속도
+    speed: 4500, // 속도
     slidesToShow: 1, // 슬라이드 표시
     slidesToScroll: 1, // 슬라이드로스크롤
     autoplay: true, // 자동재생
@@ -72,25 +77,25 @@ const Main: React.FC = () => {
                   신청하기
                 </Link>
               </div>
-              {/* <div></div> 배경화면 */}
+              <div style={{ backgroundImage: `url(${isPc ? vmImagePath : vmMobileImagePath})` }}>{/* 배경화면 */}</div>
             </MainPageSlider1>
             {/* 2번 슬라이더 _  */}
-            <MainPageSlider1>
+            <MainPageSlider2>
               <div className="flexCenter">
                 <div>
                 <span>
                   LOAD BALANCER
                   <br />
                 </span>
-                  <span style={{ fontSize: isPc ? "7rem" : "3.5rem" }}>로드밸런서</span>
+                  <span>로드밸런서</span>
                 </div>
                 {/* 신청하기 버튼 */}
                 <Link to="/loadbalancer" className="shadow_15 flexCenter">
                   신청하기
                 </Link>
               </div>
-              {/* <div></div> 배경화면 */}
-            </MainPageSlider1>
+              <div style={{ backgroundImage: `url(${LoadBalancerImagePath})` }}>{/* 배경화면 */}</div>
+            </MainPageSlider2>
           </Slider>
           {/* 재생 / 정지 버튼 */}
           <div className="slider-button-div flexCenter shadow_15" style={{ textAlign: "center" }} onClick={onPlay ? pause : play}>
--- src/routes/Main/styled.tsx
+++ src/routes/Main/styled.tsx
@@ -1,43 +1,96 @@
 import styled from "styled-components";
 
 export const MainPageStyled = styled.section`
-  height: ${({ theme }) => (theme.isPc ? "calc(100vh - 70px)" : "calc(100vh - 115px)")};
-  position: relative;
-
-  /* 슬라이더를 감싸는 Div */
-  & > div.slider-container > div:first-child > div {
     height: ${({ theme }) => (theme.isPc ? "calc(100vh - 70px)" : "calc(100vh - 115px)")};
-  }
+    position: relative;
 
-  /* 슬라이더 play, pause버튼 */
-  & > div.slider-container .slider-button-div {
-    position: absolute;
-    width: 3rem;
-    height: 3rem;
-    bottom: ${({ theme }) => (theme.isPc ? "3vh" : "10vh")};
-    right: 3vh;
-    background-color: white;
-    border-radius: 2.5rem;
-    cursor: pointer;
-  }
+    /* 슬라이더를 감싸는 Div */
+    & > div.slider-container > div:first-child > div {
+        height: ${({ theme }) => (theme.isPc ? "calc(100vh - 70px)" : "calc(100vh - 115px)")};
+    }
 
-  & > div.slider-container .slider-button-div .button {
-    font-size: 1.3rem;
-    background-color: white;
-    color: #454545;
-  }
+    /* 슬라이더 play, pause버튼 */
+    & > div.slider-container .slider-button-div {
+        position: absolute;
+        width: 3rem;
+        height: 3rem;
+        bottom: ${({ theme }) => (theme.isPc ? "3vh" : "10vh")};
+        right: 3vh;
+        background-color: white;
+        border-radius: 2.5rem;
+        cursor: pointer;
+    }
 
-  /* 슬라이더 dots */
-  & > div.slider-container .slick-dots {
-    position: absolute;
-    text-align: end;
-    bottom: ${({ theme }) => (theme.isPc ? "calc(3vh + 0.9rem)" : "calc(10vh + 0.9rem)")};
-    right: calc(3rem + 3vh + 15px);
-  }
+    & > div.slider-container .slider-button-div .button {
+        font-size: 1.3rem;
+        background-color: white;
+        color: #454545;
+    }
+
+    /* 슬라이더 dots */
+    & > div.slider-container .slick-dots {
+        position: absolute;
+        text-align: end;
+        bottom: ${({ theme }) => (theme.isPc ? "calc(3vh + 0.9rem)" : "calc(10vh + 0.9rem)")};
+        right: calc(3rem + 3vh + 15px);
+        color: white;
+    }
 `;
 
 // 슬라이더 1
 export const MainPageSlider1 = styled.section`
+    height: ${({ theme }) => (theme.isPc ? "calc(100vh - 70px)" : "calc(100vh - 115px)")};
+    width: 100vw;
+    position: relative;
+
+    & > div:first-child {
+        max-width: ${({ theme }) => (theme.isPc ? "600px" : "90%")};
+        flex-wrap: wrap;
+        position: absolute;
+        top: 50%; /* 화면의 50% 아래 */
+        left: 50%; /* 화면의 50% 오른쪽 */
+        transform: translate(-50%, -50%); /* 요소 크기만큼 반대로 이동 */
+    }
+
+    & > div:first-child > div:first-child {
+        flex-wrap: wrap;
+        text-align: center;
+        margin-bottom: ${({ theme }) => (theme.isPc ? "40px" : "15px")};
+        color: white;
+    }
+    /* background-color: blue; */
+
+    & > div:first-child > div:first-child > span:first-child {
+        font-size: ${({ theme }) => (theme.isPc ? "2.8rem" : "1.7rem")};
+    }
+
+    & > div:first-child > div:first-child > span:last-child {
+        font-size: ${({ theme }) => (theme.isPc ? "7rem" : "4.5rem")};
+        font-weight: 700;
+    }
+
+    & > div:first-child > *:nth-child(2) {
+        max-width: 500px;
+        width: ${({ theme }) => (theme.isPc ? "28vw" : "75vw")};
+        height: ${({ theme }) => (theme.isPc ? "50px" : "50px")};
+        background-color: white;
+        border-radius: 50px;
+        text-align: center;
+        margin-bottom: ${({ theme }) => (theme.isPc ? "0" : "calc(10vh + 0.9rem)")};
+        font-weight: 600;
+        cursor: pointer;
+    }
+
+    /* 백그라운드 이미지 */
+    & > div:last-child {
+        width: 100%;
+        height: 100%;
+        background: no-repeat center / cover;
+    }
+`;
+
+// 슬라이더 2
+export const MainPageSlider2 = styled.section`
   height: ${({ theme }) => (theme.isPc ? "calc(100vh - 70px)" : "calc(100vh - 115px)")};
   width: 100vw;
   position: relative;
@@ -55,6 +108,7 @@ export const MainPageSlider1 = styled.section`
     flex-wrap: wrap;
     text-align: center;
     margin-bottom: ${({ theme }) => (theme.isPc ? "40px" : "15px")};
+    color: white;
   }
   /* background-color: blue; */
 
@@ -63,7 +117,7 @@ export const MainPageSlider1 = styled.section`
   }
 
   & > div:first-child > div:first-child > span:last-child {
-    font-size: ${({ theme }) => (theme.isPc ? "7rem" : "4.5rem")};
+    font-size: ${({ theme }) => (theme.isPc ? "7rem" : "3.5rem")};
     font-weight: 700;
   }
 
@@ -80,8 +134,9 @@ export const MainPageSlider1 = styled.section`
   }
 
   /* 백그라운드 이미지 */
-  /* & > div:last-child {
+  & > div:last-child {
     width: 100%;
     height: 100%;
-  } */
+    background: no-repeat center / cover;
+  }
 `;
